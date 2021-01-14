a849fd87c9f7062c01e7979b26c32484618163af
gabriel wallace <gwbds1@gmail.com>
2021-01-14 19:20:03 -0300

    funcoes

--- exercicios/lista_exercicios_resposta/heranca2.js
+++
@@ -1,5 +0,0 @@
-// cadeia de prototipos (prototype chain)
-const avo = { attr1: 'A'}
-const pai = {__proto__: avo, attr2: 'B'}
-const filho = { __proto__: pai, attr3: 'C'}
-console.log(filho.attr3)
--- exercicios/lista_exercicios_resposta/heranca3.js
+++
@@ -1,2 +0,0 @@
-const pai = { nome: 'Pedro', corCabelo: 'preto'}
-
