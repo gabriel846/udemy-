20897ef26e2d0a6a8024c04292cdb1bc8f765495
gabriel wallace <gwbds1@gmail.com>
2021-01-21 17:25:09 -0300

    node

--- node/pastaA/pastaB/acessarModulos.js
+++ node/pastaA/pastaB/acessarModulos.js
@@ -4,6 +4,8 @@ console.log(moduloA.ola)
 const saudacao = require('saudacao')
 console.log(saudacao.ola)
 
+const c = require('')
+
 const http = require('http')
 http.createServer((req, res) => {
     res.write('bom dia')
